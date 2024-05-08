2ca8b3db9a8dec373bb1169f82d12753d560d67d
jangug0131 <jangug0131@naver.com>
2024-05-08 15:56:22 +0900

    init back

---
+++ fitnect/src/test/java/com/ssafy/fitnect/FitnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.fitnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
